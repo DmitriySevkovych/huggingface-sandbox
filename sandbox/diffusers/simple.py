f7b4f4019ef9dd8e7dcff4dd58eed962730895ab
Dmitriy Sevkovych <dmitriy@sevkovych.com>
2024-04-24 17:44:55 +0200

    chore: add cuda to project; add scripts for diffusers and transformers

--- sandbox/diffusers/simple.py
+++ sandbox/diffusers/simple.py
@@ -1,18 +1,22 @@
 import os
 
+import torch
 from diffusers import DiffusionPipeline
 
 
-def test_pipeline(prompt: str):
-    # From diffusers tutorial
-    pipeline = DiffusionPipeline.from_pretrained(
-        "./models/stable-diffusion-v1-5", use_safetensors=True
-    )
-    # pipeline.save_pretrained("./models")
-    # Newest, smallest, based off SD v2.1
-    # pipeline = DiffusionPipeline.from_pretrained("stabilityai/sdxl-turbo")
-    # pipeline = DiffusionPipeline.from_pretrained("stabilityai/sdxl-turbo")
-    # pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0")
+def _get_generator(seed: int = 0) -> torch.Generator:
+    return torch.Generator("cuda").manual_seed(seed)
 
-    image = pipeline(prompt).images[0]
+
+def test_pipeline(prompt: str):
+    checkpoint = "runwayml/stable-diffusion-v1-5"
+    pipeline = DiffusionPipeline.from_pretrained(
+        f"./models/{checkpoint}", use_safetensors=True
+    )
+    # pipeline.save_pretrained(f"./models/{checkpoint}")
+
+    pipeline = pipeline.to("cuda")
+
+    image = pipeline(prompt, generator=_get_generator()).images[0]
+
     image.save(os.path.join(os.getcwd(), "data", "diffusers_output.png"))
--- sandbox/main.py
+++ sandbox/main.py
@@ -1,9 +1,8 @@
 import logging
 
 from .app_logging.setup import setup_logging
-
-# from .transformers.inference import simple
-from .diffusers import simple
+from .diffusers.simple import test_pipeline as diffuser_pipeline
+from .transformers.inference.simple import test_pipeline as transformer_pipeline
 
 #
 # Logging
@@ -14,13 +13,20 @@ logger = logging.getLogger(__name__)
 #
 # Entry points
 #
-def start():
-    """Launched with `poetry run start` at project root level"""
+def diffuser():
+    """Launched with `poetry run diffuser` at project root level"""
     setup_logging()
-    result = simple.test_pipeline("An image of a squirrel in Picasso style")
+    result = diffuser_pipeline("An image of a squirrel in Picasso style")
+    logger.debug(result)
+
+
+def transformer():
+    """Launched with `poetry run transformer` at project root level"""
+    setup_logging()
+    result = transformer_pipeline("An image of a squirrel in Picasso style")
     logger.debug(result)
 
 
 if __name__ == "__main__":
     """Python module entry point. Is run via `python -m api.main` e.g. on Docker container start (cf. Dockerfile)"""
-    start()
+    diffuser()
