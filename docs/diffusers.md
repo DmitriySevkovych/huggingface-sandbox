e362773ea04c874839e50f0d641675cec70000bc
Dmitriy Sevkovych <dmitriy@sevkovych.com>
2024-04-26 21:30:06 +0200

    feat: generate and preview a batch of images

--- docs/diffusers.md
+++ docs/diffusers.md
@@ -3,5 +3,11 @@
 
 Helpful links, mostly from the official docs:
 
+### Basics
+
 - [How does Stable Diffusion work?](https://huggingface.co/blog/stable_diffusion#how-does-stable-diffusion-work) Explanation of how VAE, UNet, denoising scheduler and text encoder are used together.
 - [Colab going into more detail on `UNet`](https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/diffusers_intro.ipynb#scrollTo=wW8o1Wp0zRkq)
+
+### Optimisations
+
+- [Blog post on how to optimise Stable Diffusion XL pipelines](https://huggingface.co/blog/simple_sdxl_optimizations)
--- sandbox/diffusers/simple.py
+++ sandbox/diffusers/simple.py
@@ -1,7 +1,9 @@
 import os
+import random
 
 import torch
 from diffusers import DiffusionPipeline
+from diffusers.utils import make_image_grid
 
 
 def _get_generator(seed: int = 0) -> torch.Generator:
@@ -9,30 +11,41 @@ def _get_generator(seed: int = 0) -> torch.Generator:
 
 
 def test_pipeline(prompt: str):
-    seed = 123
     # checkpoint = "runwayml/stable-diffusion-v1-5"
     checkpoint = "stabilityai/stable-diffusion-xl-base-1.0"
-    precision = torch.float16
+    n = 4
 
+    seeds = [random.randint(0, 150) for _ in range(n)]
+    inputs = {
+        "prompt": n * [prompt],
+        "generator": [_get_generator(seed) for seed in seeds],
+        "num_inference_steps": 20,
+        #'guidance_scale':20.5,
+    }
+
+    # inference
     pipeline = DiffusionPipeline.from_pretrained(
         checkpoint,
-        torch_dtype=precision,
+        torch_dtype=torch.float16,
         use_safetensors=True,
     )
 
     pipeline = pipeline.to("cuda")
 
-    image = pipeline(
-        prompt,
-        generator=_get_generator(seed=seed),
-        num_inference_steps=25,
-        #  strength=0.3,
-        #  guidance_scale=20.5,
-    ).images[0]
+    if n > 1:  # To prevent OOM errors
+        # See https://huggingface.co/docs/diffusers/en/stable_diffusion#memory
+        # pipeline.enable_attention_slicing()
+        # See https://huggingface.co/blog/simple_sdxl_optimizations
+        pipeline.enable_vae_slicing()
+        pipeline.enable_sequential_cpu_offload()
 
-    output_path = os.path.join(
-        os.getcwd(), "data", f"diffusers_output_{str(precision)}_seed-{seed}.png"
-    )
+    images = pipeline(**inputs).images
 
-    image.save(output_path)
-    return f"Genetrated image {output_path}"
+    # Save and showresults
+    make_image_grid(images, 2, 2).show()
+    for i in range(n):
+        images[i].save(
+            os.path.join(os.getcwd(), "data", f"diffusers_output_seed-{seeds[i]}.png")
+        )
+
+    return f'Genetrated {n} images for prompt "{prompt}"'
