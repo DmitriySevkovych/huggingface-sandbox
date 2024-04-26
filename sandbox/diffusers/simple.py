45f803de5716d42e5be6d7834f2526a95767f1f5
Dmitriy Sevkovych <dmitriy@sevkovych.com>
2024-04-26 22:43:27 +0200

    refactor: extract DiffusionPipeline creation logic to separate method

--- sandbox/diffusers/simple.py
+++ sandbox/diffusers/simple.py
@@ -5,9 +5,26 @@ import torch
 from diffusers import DiffusionPipeline
 from diffusers.utils import make_image_grid
 
+CWD = os.getcwd()
+
 
 def _get_generator(seed: int = 0) -> torch.Generator:
     return torch.Generator("cuda").manual_seed(seed)
+
+
+def _load_pipeline(checkpoint: str) -> DiffusionPipeline:
+    args = {"torch_dtype": torch.float16, "use_safetensors": True}
+    model_path = os.path.join(CWD, "models", checkpoint)
+    if os.path.isdir(model_path):
+        pipeline = DiffusionPipeline.from_pretrained(model_path, **args)
+    else:
+        pipeline = DiffusionPipeline.from_pretrained(checkpoint, **args)
+        pipeline.save_pretrained(model_path)
+
+    if torch.cuda.is_available():
+        pipeline = pipeline.to("cuda")
+
+    return pipeline
 
 
 def test_pipeline(prompt: str):
@@ -23,14 +40,8 @@ def test_pipeline(prompt: str):
         #'guidance_scale':20.5,
     }
 
-    # inference
-    pipeline = DiffusionPipeline.from_pretrained(
-        checkpoint,
-        torch_dtype=torch.float16,
-        use_safetensors=True,
-    )
-
-    pipeline = pipeline.to("cuda")
+    # instantiate pipeline
+    pipeline = _load_pipeline(checkpoint)
 
     if n > 1:  # To prevent OOM errors
         # See https://huggingface.co/docs/diffusers/en/stable_diffusion#memory
@@ -39,13 +50,14 @@ def test_pipeline(prompt: str):
         pipeline.enable_vae_slicing()
         pipeline.enable_sequential_cpu_offload()
 
+    # inference
     images = pipeline(**inputs).images
 
     # Save and showresults
     make_image_grid(images, 2, 2).show()
     for i in range(n):
         images[i].save(
-            os.path.join(os.getcwd(), "data", f"diffusers_output_seed-{seeds[i]}.png")
+            os.path.join(CWD, "data", f"diffusers_output_seed-{seeds[i]}.png")
         )
 
     return f'Genetrated {n} images for prompt "{prompt}"'
